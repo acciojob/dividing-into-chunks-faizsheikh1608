021597d644dd43886905a59e7d24bcd9174d55c2
faizsheikh1608 <164844564+faizsheikh1608@users.noreply.github.com>
2024-06-06 11:48:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,5 +18,5 @@ const divide = (arr, n) => {
 	return ans
 };
 
-//const n = prompt("Enter n: ");
-//alert(JSON.stringify(divide(arr, n)));
+const n = prompt("Enter n: ");
+alert(JSON.stringify(divide(arr, n)));
